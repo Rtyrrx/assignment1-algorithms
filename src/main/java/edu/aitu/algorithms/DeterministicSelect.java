8ffd5d56f33d78542415e64f3cce8095286f326b
Rtyrrx <wwertyusklye@gmail.com>
2025-09-28 21:17:30 +0500

    feat(select): added skeleton

---
+++ src/main/java/edu/aitu/algorithms/DeterministicSelect.java
@@ -0,0 +1,8 @@
+package edu.aitu.algorithms;
+
+public class DeterministicSelect {
+    public int select(int[] arr, int k) {
+        // TODO: implement median-of-medians select (O(n))
+        return -1; // placeholder
+    }
+}
