a6f9cb2379b7525f5df30ef6fb41678081978943
Rtyrrx <wwertyusklye@gmail.com>
2025-09-28 21:26:22 +0500

    feat(closest): added skeleton

---
+++ src/main/java/edu/aitu/algorithms/ClosestPair.java
@@ -0,0 +1,8 @@
+package edu.aitu.algorithms;
+
+public class ClosestPair {
+    public double findClosest(double[][] points) {
+        // TODO: implement closest pair of points in 2D (O(n log n))
+        return 0.0; // placeholder
+    }
+}
